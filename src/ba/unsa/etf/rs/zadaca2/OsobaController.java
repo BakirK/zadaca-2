b96cba680c1723aff70bfcb631a02f9d968888d7
Bakir Karovic <bakir.karovic@gmail.com>
2019-04-20 14:47:44 +0200

    importovani elemennti forme preko njihovog fx:id u OsobeController

--- src/ba/unsa/etf/rs/zadaca2/OsobaController.java
+++ src/ba/unsa/etf/rs/zadaca2/OsobaController.java
@@ -1,11 +1,35 @@
 package ba.unsa.etf.rs.zadaca2;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
 public class OsobaController {
     private OsobeModel model;
+    @FXML
+    private TableView tabelaOsobe;
+    @FXML
+    private Label imeText;
+    @FXML
+    private Label prezimeText;
+    @FXML
+    private Label ulicaText;
+    @FXML
+    private Label gradText;
+    @FXML
+    private Label postanskiBrojText;
+    @FXML
+    private Label rodjendanText;
+    @FXML
+    private Button btnDodaj;
+    @FXML
+    private Button btnObrisi;
 
     public OsobaController(OsobeModel modelInput) {
         model = modelInput;
     }
+    
 
 
 
