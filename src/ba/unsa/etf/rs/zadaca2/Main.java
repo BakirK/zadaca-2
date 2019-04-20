9ebae5b5d860dccf3da21a8bdb1660436f1b43b3
Bakir Karovic <bakir.karovic@gmail.com>
2019-04-20 13:00:14 +0200

    Dodana dugmad dodaj i obrisi te ikone u njima

--- src/ba/unsa/etf/rs/zadaca2/Main.java
+++ src/ba/unsa/etf/rs/zadaca2/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         OsobeModel model = new OsobeModel();
         model.napuni();
         OsobaController ctrl = new OsobaController(model);
