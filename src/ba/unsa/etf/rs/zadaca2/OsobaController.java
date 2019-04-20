f9a1635065c4c97bbcf8c0bda200d82e699141cc
Bakir Karovic <bakir.karovic@gmail.com>
2019-04-20 14:26:07 +0200

    Dodana metoda OsobaController

--- src/ba/unsa/etf/rs/zadaca2/OsobaController.java
+++ src/ba/unsa/etf/rs/zadaca2/OsobaController.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rs.zadaca2;
 
 public class OsobaController {
+    private OsobeModel model;
+
+    public OsobaController(OsobeModel modelInput) {
+        model = modelInput;
+    }
+
+
+
 }
