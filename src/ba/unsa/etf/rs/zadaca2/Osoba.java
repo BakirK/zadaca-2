b1a1655a368761a534d8488808e80e717c29aed9
Bakir Karovic <bakir.karovic@gmail.com>
2019-04-20 13:11:55 +0200

    Dodani atributi klase Osoba

--- src/ba/unsa/etf/rs/zadaca2/Osoba.java
+++ src/ba/unsa/etf/rs/zadaca2/Osoba.java
@@ -1,4 +1,18 @@
 package ba.unsa.etf.rs.zadaca2;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.time.LocalDate;
+
 public class Osoba {
+    private SimpleStringProperty ime = new SimpleStringProperty("<Novi korisnik>");
+    private SimpleStringProperty prezime = new SimpleStringProperty("<Novi korisnik>");
+    private SimpleStringProperty ulica = new SimpleStringProperty("<Novi korisnik>");
+    private SimpleStringProperty grad = new SimpleStringProperty("<Novi korisnik>");
+    private SimpleIntegerProperty postanskiBroj = new SimpleIntegerProperty();
+    private SimpleObjectProperty<LocalDate> rodjendan = new SimpleObjectProperty<LocalDate>();
+
+
 }
