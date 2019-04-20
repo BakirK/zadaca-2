87ad7ea84de0a226d0f07b572835146215bbfe25
Bakir Karovic <bakir.karovic@gmail.com>
2019-04-20 13:46:18 +0200

    Premjesten sadrzaj klasa iz OsobaController u OsobaModel

--- src/ba/unsa/etf/rs/zadaca2/OsobaController.java
+++ src/ba/unsa/etf/rs/zadaca2/OsobaController.java
@@ -1,34 +1,4 @@
 package ba.unsa.etf.rs.zadaca2;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
-import javafx.collections.ObservableList;
-import javafx.collections.FXCollections;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-
 public class OsobaController {
-    private ObservableList<Osoba> osoba = FXCollections.observableArrayList();
-    private SimpleObjectProperty trenutnaOsoba = new SimpleObjectProperty();
-
-
-    private ObservableList<Osoba> getOsoba() {
-        return osoba;
-    }
-
-    private void setOsoba(ObservableList<Osoba> osoba) {
-        this.osoba = osoba;
-    }
-
-    private Object getTrenutnaOsoba() {
-        return trenutnaOsoba.get();
-    }
-
-    public SimpleObjectProperty trenutnaOsobaProperty() {
-        return trenutnaOsoba;
-    }
-
-    private void setTrenutnaOsoba(Object trenutnaOsoba) {
-        this.trenutnaOsoba.set(trenutnaOsoba);
-    }
 }
--- src/ba/unsa/etf/rs/zadaca2/OsobeModel.java
+++ src/ba/unsa/etf/rs/zadaca2/OsobeModel.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rs.zadaca2;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class OsobeModel {
+
+        private ObservableList<Osoba> osoba = FXCollections.observableArrayList();
+        private SimpleObjectProperty trenutnaOsoba = null;
+
+
+        private ObservableList<Osoba> getOsoba() {
+            return osoba;
+        }
+
+        private void setOsoba(ObservableList<Osoba> osoba) {
+            this.osoba = osoba;
+        }
+
+        private Object getTrenutnaOsoba() {
+            return trenutnaOsoba.get();
+        }
+
+        public SimpleObjectProperty trenutnaOsobaProperty() {
+            return trenutnaOsoba;
+        }
+
+        private void setTrenutnaOsoba(Object trenutnaOsoba) {
+            this.trenutnaOsoba.set(trenutnaOsoba);
+        }
+
+        private void napuni () {
+//            osoba.add(new Osoba)
+        }
 }
+
+
