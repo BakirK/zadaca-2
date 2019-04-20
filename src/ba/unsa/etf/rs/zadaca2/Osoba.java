4a70fac4beadf66da063b1c9edf15a88e6653c4d
Bakir Karovic <bakir.karovic@gmail.com>
2019-04-20 13:13:08 +0200

    Dodani getteri i setteri za atribute klase Osoba

--- src/ba/unsa/etf/rs/zadaca2/Osoba.java
+++ src/ba/unsa/etf/rs/zadaca2/Osoba.java
@@ -15,4 +15,80 @@ public class Osoba {
     private SimpleObjectProperty<LocalDate> rodjendan = new SimpleObjectProperty<LocalDate>();
 
 
+    private String getIme() {
+        return ime.get();
+    }
+
+    public SimpleStringProperty imeProperty() {
+        return ime;
+    }
+
+    private void setIme(String ime) {
+        this.ime.set(ime);
+    }
+
+
+    private String getPrezime() {
+        return prezime.get();
+    }
+
+    public SimpleStringProperty prezimeProperty() {
+        return prezime;
+    }
+
+    private void setPrezime(String prezime) {
+        this.prezime.set(prezime);
+    }
+
+
+    private String getUlica() {
+        return ulica.get();
+    }
+
+    public SimpleStringProperty ulicaProperty() {
+        return ulica;
+    }
+
+    private void setUlica(String ulica) {
+        this.ulica.set(ulica);
+    }
+
+
+    private String getGrad() {
+        return grad.get();
+    }
+
+    public SimpleStringProperty gradProperty() {
+        return grad;
+    }
+
+    private void setGrad(String grad) {
+        this.grad.set(grad);
+    }
+
+
+    private int getPostanskiBroj() {
+        return postanskiBroj.get();
+    }
+
+    public SimpleIntegerProperty postanskiBrojProperty() {
+        return postanskiBroj;
+    }
+
+    private void setPostanskiBroj(int postanskiBroj) {
+        this.postanskiBroj.set(postanskiBroj);
+    }
+
+
+    private LocalDate getRodjendan() {
+        return rodjendan.get();
+    }
+
+    public SimpleObjectProperty<LocalDate> rodjendanProperty() {
+        return rodjendan;
+    }
+
+    private void setRodjendan(LocalDate rodjendan) {
+        this.rodjendan.set(rodjendan);
+    }
 }
